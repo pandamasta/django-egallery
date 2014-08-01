87a4bdd2aa71ce34b250dd86bbbe7a8e4f403d59
Aurelien <panda@pandabook-2.local>
2014-08-01 16:52:05 +0200

    Add function to remove pictures and related thumbs by bulk in the admin backend

--- egallery/admin.py
+++ egallery/admin.py
@@ -3,16 +3,24 @@ from django.contrib import admin
 # Register your models here.
 from egallery.models import *
 
-class CategoryAdmin(admin.ModelAdmin): 
+# Funtion to delete selected picture and its related thumb
+def delete_model(modeladmin, request, queryset):
+    for obj in queryset:
+        storage, path = obj.picture.storage, obj.picture.path
+        obj.picture.delete_thumbnails()
+        storage.delete(path) #delete original picture
+        obj.delete()
+delete_model.short_description = "Delete selected picture and its thumbs"
+
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title','is_public','description','created']
     prepopulated_fields = {"slug": ("title",)}
 
-
-class PictureAdmin(admin.ModelAdmin): 
+class PictureAdmin(admin.ModelAdmin):
     list_display = ['title','image_img','description','created','gallery']
     list_filter = ('gallery',)
     prepopulated_fields = {"slug": ("title",)}
-
+    actions = [delete_model]
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Picture,PictureAdmin)
